42eb1a8d425e04fc26705695873f58fece251a55
KramerZJ <zijianwanggamedev@gmail.com>
2022-11-12 15:31:05 -0500

    basic test UI
    Need to fix spawner and the list of the Enemy in class : PlayerProjectileSpawner

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -7,6 +7,7 @@ public class CharacterMovement : MonoBehaviour
     Vector2 direction;
     [SerializeField] float speed = 15f;
     [SerializeField] Rigidbody2D rb;
+    Vector3 mousePostion;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +17,18 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direction.y = Input.GetAxis("Vertical");
-        direction.x = Input.GetAxis("Horizontal");      
+        mousePostion = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = new Vector2(mousePostion.x - transform.position.x, mousePostion.y - transform.position.y).normalized;
+        if (Mathf.Abs(mousePostion.x - transform.position.x)+Mathf.Abs(mousePostion.y - transform.position.y) < 1f)
+        {
+                direction.x = 0f;
+                direction.y = 0f;
+        }
+        
     }
     private void FixedUpdate()
     {
+        //Debug.Log(mousePostion.x - transform.position.x);
         rb.velocity = direction * speed;
     }
 }
